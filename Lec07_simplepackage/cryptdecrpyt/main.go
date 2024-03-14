df498ba8ed0342eeed3f883d22bd8c27b5de41c1
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-03-14 20:56:51 +0700

    ✅ add: package

--- Lec07_simplepackage/cryptdecrpyt/main.go
+++ Lec07_simplepackage/cryptdecrpyt/main.go
@@ -1,5 +1,14 @@
 package cryptdecrpyt
 
-func main() {
+func Encrypt(text string, key string) string {
+	data := []byte(text)
+	for i := range data {
+		data[i] ^= key[i%len(key)]
+	}
+	return string(data)
 
+}
+
+func Decrypt_text(text string, key string) string {
+	return Encrypt(text, key)
 }
